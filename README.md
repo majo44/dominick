c3e4c2e2d6216c6abad12da1456b6cdd03a0940d
majo44 <majo44@gmail.com>
2019-02-19 11:50:15 +0300

    Readme & todo example.

--- README.md
+++ README.md
@@ -1,1 +1,23 @@
-# dominick
+# @dominick
+Small library for html rendering by using tagged string templates & virtual dom.
+*This code is experimental*
+
+### The assumptions
+* No custom DSL, plain string literals and javascript
+* No compilation/transpiration requirement  
+* Support SSR 
+* Support lazy load
+* Simple functional computerisation with support of lifecycle hooks
+* Minimal size, efficient render, minimal memory consumption  
+* ...
+
+### Example 1 
+```javascript
+const component = ()
+
+```
+
+### Todo
+* Coverage
+* Example with routing
+* Example with I18n
---
+++ examples/todo/app.js
@@ -0,0 +1,69 @@
+import { shouldUpdate, h, useState, useRef } from "../../packages/core/index.js";
+import { render } from "../../packages/dom/index.js";
+
+const itemComponent = (item, done, remove) => () => {
+    shouldUpdate(item);
+    return h`<tr>
+        <td>
+            ${item.done ? h`<s>${item.title}</s>` : item.title}
+        </td>
+        <td><button onclick="${() => done(item)}">${item.done ? 'undone' : 'done'}</button></td>
+        <td><button onclick="${() => remove(item)}">remove</button></td>        
+    </tr>`;
+};
+
+const form = (create) => () => {
+    const [name, setName] = useState('');
+    const input = useRef();
+    const onkeyup = (e) => {
+        const newValue = input.current.value;
+        if (e.key === "Enter") {
+            create(newValue);
+            setName('');
+        } else if (newValue !== name) {
+            setName(newValue);
+        }
+    };
+    return h`<input ref="${input}" onkeyup="${onkeyup}" value="${name}"/>`
+};
+
+const app = (items, create, done, remove) => () =>
+    h`<div>
+        <h1>Todo app</h1>
+        ${form(create)}
+        <table>
+            ${items.map(i => itemComponent(i, done, remove))}
+        </table>
+    </div>`;
+
+let items = [];
+
+const create = (title) => {
+    items = [{title},...items];
+    renderApp();
+};
+
+const done = (item) => {
+    items = items.map((i) => {
+       if (i === item) {
+           return {
+               ...i, done: !i.done
+           }
+       } else {
+           return i;
+       }
+    });
+    renderApp();
+};
+
+
+const remove = (item) => {
+    items = items.filter((i) => i !== item);
+    renderApp();
+};
+
+const renderApp = () => {
+    render(document.body, app(items, create, done, remove));
+};
+
+renderApp();
